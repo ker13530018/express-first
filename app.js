77713a3647cb7ffa1eb6e5c2bc87b14a3584b2ce
JIrayut Khantavee <ker.koleng@hotmail.co.th>
2018-08-20 17:36:23 +0700

    [*] Remove space

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ const bodyParser = require('body-parser')
 const oauth = require('./api/oauth')
 
 const app = express()
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-
 app.use('/api/v1/oauth', oauth)
-
 
 module.exports = app
